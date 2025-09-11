e0ca0fe27bb90ff1987bcbe9128d2f8be8bc34ba
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2025-09-11 23:14:10 +0500

    code updated

--- PythonCourse/Lecture05/main.py
+++ PythonCourse/Lecture05/main.py
@@ -1,2 +1,21 @@
-for a in range(90,101,2):
-    print("Index ",a)
+
+
+
+
+
+# for  a in range(90,100,2):
+#     print("Index",a)
+
+
+for a in range(100):
+    if a % 2==0:
+        print("Even ",a)
+    else:
+        print("Odd ",a)
+
+
+num =int(input("Enter Your Number"))
+merafactorial=1
+for b in range(1,num+1):
+    merafactorial*=b
+print("Factorial is ",merafactorial)
