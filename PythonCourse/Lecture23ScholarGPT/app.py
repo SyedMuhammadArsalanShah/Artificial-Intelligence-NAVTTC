35858d9dfa77ea5dc4d8896487646eeb7ff7214d
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2025-10-29 23:03:46 +0500

    Code

--- PythonCourse/Lecture23ScholarGPT/app.py
+++ PythonCourse/Lecture23ScholarGPT/app.py
@@ -12,4 +12,6 @@ def home():
     return render_template("index.html", answer=answer)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    # for hosting ----
+    app.run(host="0.0.0.0", port=5000)
